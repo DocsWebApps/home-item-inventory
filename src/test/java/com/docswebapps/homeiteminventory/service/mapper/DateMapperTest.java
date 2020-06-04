446d5f84a3a2687b2a8b82f57507d8becae9e82f
Dave Collier <lardelbow@gmail.com>
2020-06-04 15:56:36 +0100

    Added DateMapperTest - all tests passing

---
+++ src/test/java/com/docswebapps/homeiteminventory/service/mapper/DateMapperTest.java
@@ -0,0 +1,32 @@
+package com.docswebapps.homeiteminventory.service.mapper;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DateMapperTest {
+    DateMapper dateMapper = new DateMapper();
+
+    @Test
+    @DisplayName("asOffsetDateTime()")
+    void asOffsetDateTime() {
+        OffsetDateTime offsetDateTime = dateMapper.asOffsetDateTime(Timestamp.valueOf(LocalDateTime.now()));
+        assertNotNull(offsetDateTime);
+        assertTrue(offsetDateTime instanceof OffsetDateTime);
+    }
+
+    @Test
+    @DisplayName("asTimestamp()")
+    void asTimestamp() {
+        Timestamp timestamp = dateMapper.asTimestamp(OffsetDateTime.now(ZoneId.systemDefault()));
+        assertNotNull(timestamp);
+        assertTrue(timestamp instanceof Timestamp);
+    }
+}
