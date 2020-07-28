38681dab34e5277ab8e90affc597d4c20ffe684a
Dave Collier <lardelbow@gmail.com>
2020-07-28 13:50:59 +0100

    Added in memory user authentication

--- src/main/java/com/docswebapps/homeiteminventory/config/WebSecurityConfig.java
+++ src/main/java/com/docswebapps/homeiteminventory/config/WebSecurityConfig.java
@@ -1,9 +1,16 @@
 package com.docswebapps.homeiteminventory.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -15,15 +22,48 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.headers().frameOptions().disable(); // Required to use H2 in memory DB - remove for Prod
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/webjars/**").permitAll()
-                .antMatchers("/css/**").permitAll()
-                .antMatchers("/img/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/").permitAll()
+                .antMatchers(HttpMethod.GET,"/webjars/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/css/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/img/**").permitAll()
                 .antMatchers(API_LOCATION_URL_V1+"/**").permitAll()
                 .antMatchers(WEB_LOCATION_URL+"/**").permitAll()
                 .anyRequest().authenticated()
                 .and().cors()
                 .and().csrf().disable();
+    }
+
+
+    // ************ Set up users for in memory authentication
+
+    // Method1: Fluent API
+    // {noop} - used to not encrypt password ie: .password("{noop}admin")
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password("{noop}admin")
+            .roles("ADMIN")
+            .and()
+            .withUser("user")
+            .password("{noop}user")
+            .roles("USER");
+    }
+
+    // Method2: Configure the UserDetailsService
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN").build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("user")
+                .roles("USER").build();
+
+        return new InMemoryUserDetailsManager(admin,user);
     }
 }
 
