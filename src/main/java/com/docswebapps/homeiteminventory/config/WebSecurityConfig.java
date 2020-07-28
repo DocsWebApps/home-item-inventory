b1ebe69f2326ec80651e90c7ee957c6bd17217b2
Dave Collier <lardelbow@gmail.com>
2020-07-28 15:22:28 +0100

    HttpBasic tests part 2

--- src/main/java/com/docswebapps/homeiteminventory/config/WebSecurityConfig.java
+++ src/main/java/com/docswebapps/homeiteminventory/config/WebSecurityConfig.java
@@ -1,15 +1,11 @@
 package com.docswebapps.homeiteminventory.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -38,32 +34,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //
 //    // Method1: Fluent API
 //    // {noop} - used to not encrypt password ie: .password("{noop}admin")
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//            .withUser("admin")
-//            .password("{noop}admin")
-//            .roles("ADMIN")
-//            .and()
-//            .withUser("user")
-//            .password("{noop}user")
-//            .roles("USER");
-//    }
-//
-//    // Method2: Configure the UserDetailsService
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("admin")
-                .roles("ADMIN").build();
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("user")
-                .roles("USER").build();
-
-        return new InMemoryUserDetailsManager(admin,user);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password("{noop}admin")
+            .roles("ADMIN")
+            .and()
+            .withUser("user")
+            .password("{noop}user")
+            .roles("USER");
     }
+//
+////    // Method2: Configure the UserDetailsService
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("admin")
+//                .roles("ADMIN").build();
+//
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("user")
+//                .roles("USER").build();
+//
+//        return new InMemoryUserDetailsManager(admin,user);
+//    }
 }
 
